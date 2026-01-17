ef5882c9bb02a2a7df738dadcc3ef7ce9562ce58
Israel Belete <ibelete2000@gmail.com>
2026-01-17 10:31:48 -0700

    2nd commit

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
+  isNewUser: boolean = true;
+  userObj: User = new User();
 
+  changeView() {
+    this.isNewUser = !this.isNewUser;
+  }
+}
+
+
+class User {
+  constructor(
+    public userId: number = 0,
+    public fName: string = '',
+    public lName: string = '',
+    public uName: string = '',
+    public city: string = '',
+    public state: string = '',
+    public zipCode: string = '',
+    public isAgree: boolean = false
+  ) {}
 }
